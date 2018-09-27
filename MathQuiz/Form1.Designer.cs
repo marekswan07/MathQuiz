eb70aba56e4d1d2d544bcb123c7626dc8db65498
marekswan07 <marekswan07@hotmail.com>
2018-09-26 21:02:02 -0600

    UI elements added in, Start button has some functionality. Addition variables inserted, rest off the functions to be added

--- MathQuiz/Form1.Designer.cs
+++ MathQuiz/Form1.Designer.cs
@@ -28,20 +28,336 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.lblTimeLeft = new System.Windows.Forms.Label();
+            this.addLeftLabel = new System.Windows.Forms.Label();
+            this.addRightLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.sumAdd = new System.Windows.Forms.NumericUpDown();
+            this.sumMinus = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.minusRightLabel = new System.Windows.Forms.Label();
+            this.minusLeftLabel = new System.Windows.Forms.Label();
+            this.sumDiv = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.divRightLabel = new System.Windows.Forms.Label();
+            this.divLeftLabel = new System.Windows.Forms.Label();
+            this.sumMult = new System.Windows.Forms.NumericUpDown();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.multRightLabel = new System.Windows.Forms.Label();
+            this.multLeftLabel = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.sumAdd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sumMinus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sumDiv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sumMult)).BeginInit();
             this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(208, 21);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(200, 30);
+            this.timeLabel.TabIndex = 0;
+            // 
+            // lblTimeLeft
+            // 
+            this.lblTimeLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimeLeft.Location = new System.Drawing.Point(89, 21);
+            this.lblTimeLeft.Name = "lblTimeLeft";
+            this.lblTimeLeft.Size = new System.Drawing.Size(113, 30);
+            this.lblTimeLeft.TabIndex = 1;
+            this.lblTimeLeft.Text = "Time Left: ";
+            this.lblTimeLeft.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // addLeftLabel
+            // 
+            this.addLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addLeftLabel.Location = new System.Drawing.Point(56, 82);
+            this.addLeftLabel.Name = "addLeftLabel";
+            this.addLeftLabel.Size = new System.Drawing.Size(60, 50);
+            this.addLeftLabel.TabIndex = 2;
+            this.addLeftLabel.Text = "?";
+            this.addLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // addRightLabel
+            // 
+            this.addRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addRightLabel.Location = new System.Drawing.Point(188, 82);
+            this.addRightLabel.Name = "addRightLabel";
+            this.addRightLabel.Size = new System.Drawing.Size(60, 50);
+            this.addRightLabel.TabIndex = 3;
+            this.addRightLabel.Text = "?";
+            this.addRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(122, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 50);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "+";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(254, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 50);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "=";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sumAdd
+            // 
+            this.sumAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sumAdd.Location = new System.Drawing.Point(320, 91);
+            this.sumAdd.MaximumSize = new System.Drawing.Size(100, 0);
+            this.sumAdd.Name = "sumAdd";
+            this.sumAdd.Size = new System.Drawing.Size(100, 35);
+            this.sumAdd.TabIndex = 2;
+            // 
+            // sumMinus
+            // 
+            this.sumMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sumMinus.Location = new System.Drawing.Point(320, 141);
+            this.sumMinus.MaximumSize = new System.Drawing.Size(100, 0);
+            this.sumMinus.Name = "sumMinus";
+            this.sumMinus.Size = new System.Drawing.Size(100, 35);
+            this.sumMinus.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(254, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 50);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "=";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(122, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 50);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "-";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // minusRightLabel
+            // 
+            this.minusRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusRightLabel.Location = new System.Drawing.Point(188, 132);
+            this.minusRightLabel.Name = "minusRightLabel";
+            this.minusRightLabel.Size = new System.Drawing.Size(60, 50);
+            this.minusRightLabel.TabIndex = 8;
+            this.minusRightLabel.Text = "?";
+            this.minusRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // minusLeftLabel
+            // 
+            this.minusLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusLeftLabel.Location = new System.Drawing.Point(56, 132);
+            this.minusLeftLabel.Name = "minusLeftLabel";
+            this.minusLeftLabel.Size = new System.Drawing.Size(60, 50);
+            this.minusLeftLabel.TabIndex = 7;
+            this.minusLeftLabel.Text = "?";
+            this.minusLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sumDiv
+            // 
+            this.sumDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sumDiv.Location = new System.Drawing.Point(320, 191);
+            this.sumDiv.MaximumSize = new System.Drawing.Size(100, 0);
+            this.sumDiv.Name = "sumDiv";
+            this.sumDiv.Size = new System.Drawing.Size(100, 35);
+            this.sumDiv.TabIndex = 4;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(254, 182);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 50);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "=";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(122, 182);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(60, 50);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "÷";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // divRightLabel
+            // 
+            this.divRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divRightLabel.Location = new System.Drawing.Point(188, 182);
+            this.divRightLabel.Name = "divRightLabel";
+            this.divRightLabel.Size = new System.Drawing.Size(60, 50);
+            this.divRightLabel.TabIndex = 13;
+            this.divRightLabel.Text = "?";
+            this.divRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // divLeftLabel
+            // 
+            this.divLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.divLeftLabel.Location = new System.Drawing.Point(56, 182);
+            this.divLeftLabel.Name = "divLeftLabel";
+            this.divLeftLabel.Size = new System.Drawing.Size(60, 50);
+            this.divLeftLabel.TabIndex = 12;
+            this.divLeftLabel.Text = "?";
+            this.divLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // sumMult
+            // 
+            this.sumMult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sumMult.Location = new System.Drawing.Point(320, 241);
+            this.sumMult.MaximumSize = new System.Drawing.Size(100, 0);
+            this.sumMult.Name = "sumMult";
+            this.sumMult.Size = new System.Drawing.Size(100, 35);
+            this.sumMult.TabIndex = 5;
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(233, 232);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(60, 50);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "=";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label14
+            // 
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(122, 232);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(60, 50);
+            this.label14.TabIndex = 19;
+            this.label14.Text = "x";
+            this.label14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // multRightLabel
+            // 
+            this.multRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multRightLabel.Location = new System.Drawing.Point(188, 232);
+            this.multRightLabel.Name = "multRightLabel";
+            this.multRightLabel.Size = new System.Drawing.Size(60, 50);
+            this.multRightLabel.TabIndex = 18;
+            this.multRightLabel.Text = "?";
+            this.multRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // multLeftLabel
+            // 
+            this.multLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.multLeftLabel.Location = new System.Drawing.Point(56, 232);
+            this.multLeftLabel.Name = "multLeftLabel";
+            this.multLeftLabel.Size = new System.Drawing.Size(60, 50);
+            this.multLeftLabel.TabIndex = 17;
+            this.multLeftLabel.Text = "?";
+            this.multLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStart
+            // 
+            this.btnStart.AutoSize = true;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(158, 304);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(144, 34);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start The Quiz!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.sumMult);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.multRightLabel);
+            this.Controls.Add(this.multLeftLabel);
+            this.Controls.Add(this.sumDiv);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.divRightLabel);
+            this.Controls.Add(this.divLeftLabel);
+            this.Controls.Add(this.sumMinus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.minusRightLabel);
+            this.Controls.Add(this.minusLeftLabel);
+            this.Controls.Add(this.sumAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.addRightLabel);
+            this.Controls.Add(this.addLeftLabel);
+            this.Controls.Add(this.lblTimeLeft);
+            this.Controls.Add(this.timeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Marek Swan Math quiz";
+            ((System.ComponentModel.ISupportInitialize)(this.sumAdd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sumMinus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sumDiv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sumMult)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label lblTimeLeft;
+        private System.Windows.Forms.Label addLeftLabel;
+        private System.Windows.Forms.Label addRightLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown sumAdd;
+        private System.Windows.Forms.NumericUpDown sumMinus;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label minusRightLabel;
+        private System.Windows.Forms.Label minusLeftLabel;
+        private System.Windows.Forms.NumericUpDown sumDiv;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label divRightLabel;
+        private System.Windows.Forms.Label divLeftLabel;
+        private System.Windows.Forms.NumericUpDown sumMult;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label multRightLabel;
+        private System.Windows.Forms.Label multLeftLabel;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- MathQuiz/Form1.cs
+++ MathQuiz/Form1.cs
@@ -12,9 +12,62 @@ namespace MathQuiz
 {
     public partial class Form1 : Form
     {
+        //Adding randomizer obejet
+        Random randomizer = new Random();
+
+        //int vars for all the problems
+        int addend1;
+        int addend2;
+
+        //timer variable
+        int timeLeft;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void StartTheQuiz()
+        {
+            //values assinged to the math problems
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            //converts ints to Strings and displays
+            addLeftLabel.Text = addend1.ToString();
+            addRightLabel.Text = addend2.ToString();
+
+            //ensures clear values before adding to it
+            sumAdd.Value = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // event handler /listener for when the button is pressed
+            StartTheQuiz();
+            btnStart.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //handler for the timer
+            if (timeLeft > 0)
+            {
+                // Display the new time left
+                // by updating the Time Left label.
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft + " seconds";
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer, show
+                // a MessageBox, and fill in the answers.
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sumAdd.Value = addend1 + addend2;
+                btnStart.Enabled = true;
+            }
         }
     }
 }
